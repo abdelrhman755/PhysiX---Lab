48499ddec976acbe48e67b0cf13989a7a0e09601
Abdelrhman <abdelrazkforman966@gmail.com>
2026-07-13 21:54:19 +0300

    i copied the html code of the calculators and changed the section in it and i wrote the css for all the coming simulations pages and i began in the js of the projectile simulation

---
+++ js/projectile.js
@@ -0,0 +1,8 @@
+//my variables that i will use to draw the projectile
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const speedInput = document.getElementById("speed");
+const angleInput = document.getElementById("angle");
+const launchBtn = document.getElementById("start");
+
+
