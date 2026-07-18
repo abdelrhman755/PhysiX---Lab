8c1287dd42b4fa44f9455c34942c7deb7639c832
Abdelrhman <abdelrazkforman966@gmail.com>
2026-07-18 08:44:08 +0300

    finishing the last part of the pendulum simulation by js and fix some errors that happend in the code

--- js/pendulum.js
+++ js/pendulum.js
@@ -29,7 +29,7 @@ function startSimulation (){
 
     // convert the inputs values to numbers to work in it
     const length = Number(lengthInput.value);
-    const angle = Number(angleInput.value);
+    const maxAngle = Number(angleInput.value) * Math.PI / 180;
 
     // the valid length must be greater than zero
     if(length <= 0) {
@@ -39,10 +39,11 @@ function startSimulation (){
 
     // calculate the pendulum period
     const period = 2 * Math.PI * Math.sqrt(length / g);
-    period.textContent = period.toFixed(2);
+    periodBtn.textContent = period.toFixed(2);
 
     // convert meters to pixels 
-    const ropeLength = length * 180;
+    const maxLength = canvas.height - pivoty - 30;
+    const ropeLength = Math.min(length * 25, maxLength);
 
     //angular frequency
     const omega = Math.sqrt(g / length);
@@ -60,7 +61,7 @@ function startSimulation (){
         drawCeiling();
 
         //pendulum angle
-        const theta = maxAngle * Math.cos(omega * time);
+        const theta = maxAngle * Math.cos(omega * t);
 
         //position
         const x = pivotx + ropeLength * Math.sin(theta);
@@ -71,10 +72,69 @@ function startSimulation (){
         drawB(x,y);
 
         //increase time
-        time += 0.02;
+        t += 0.02;
 
         //continue animation
         animation = requestAnimationFrame(animate);
     }
 }
 
+// function that will draw the ceiling
+function drawCeiling () {
+    ctx.beginPath();
+    ctx.moveTo(0, pivoty);
+    ctx.lineTo(canvas.width, pivoty);
+    ctx.strokeStyle = "#888";
+    ctx.lineWidth = 4;
+    ctx.stroke();
+}
+
+
+// the function that will draw the rope
+function drawR(x,y) {
+    ctx.beginPath();
+    ctx.moveTo(pivotx, pivoty);
+    ctx.lineTo(x,y);
+
+    ctx.strokeStyle = "#ffffff";
+    ctx.lineWidth = 3;
+    ctx.stroke();
+}
+
+
+// writing the function that will draw the pendulum bob
+function drawB(x,y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2);
+    ctx.fillStyle = "#3abff8";
+    ctx.shadowBlur = 20;
+    ctx.shadowColor = "#3abff8";
+    ctx.fill();
+    ctx.shadowBlur = 0;
+
+    //draw the pivot
+    ctx.beginPath();
+    ctx.arc(pivotx, pivoty, 6, 0, Math.PI * 2);
+    ctx.fillStyle = "#fc3d21";
+    ctx.fill();
+}
+
+// writing the function that will draw the grid
+function drawGrid () {
+    ctx.strokeStyle = "rgba(255,255,255,0.08)";
+    ctx.lineWidth = 1;
+    for (let x = 0; x <= canvas.width; x += 40) {
+        ctx.beginPath();
+        ctx.moveTo(x , 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
+    }
+
+
+    for (let y = 0; y <= canvas.height; y += 40) {
+        ctx.beginPath();
+        ctx.moveTo(0,y);
+        ctx.lineTo(canvas.width, y);
+        ctx.stroke();
+    }
+}
